6f394b255686af3185205950e21c2b9ce5c462e7
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-13 18:38:02 +0530

    The Site Class

--- ssg.py
+++ ssg.py
@@ -1,9 +1,9 @@
 import typer
-from ssg import site
+from ssg import Site
 
 def main(source = "content", dest = "dist"):
     config = {"source": source, "dest": dest}
-    obj_site = site(**config)
+    obj_site = Site(**config)
     obj_site.build()
 
 typer.run(main)
