0cfb9719bcb855602ffb9f6bbe59db473f4ebd27
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-13 18:49:07 +0530

    the site class

--- ssg/site.py
+++ ssg/site.py
@@ -7,12 +7,12 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        self.directory = self.dest / path.relative_to(self.source)
-        self.directory.mkdir(parents=True, exist_ok=True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-        pathlist = self.source.glob('**/*.asm')
+        pathlist = self.source.rglob("*")
         for path in pathlist:
             if path.is_dir():
                 self.create_dir(path)
