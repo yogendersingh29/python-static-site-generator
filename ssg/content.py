fddfed12fd195cff7ae96bd8a1343c7c3e5f4c96
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-14 14:52:59 +0530

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,34 @@
+import re
+from yaml import load, FullLoader
+
+class Content(Mapping):
+
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader = FullLoader)
+        return cls(metadata, content)
+    
+    def __init__ (self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+    
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+    
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+            
+
