631c9cb949766c1720fb6b3ac9a0e158ba98edee
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-14 16:06:58 +0530

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -30,5 +30,18 @@ class Content(Mapping):
 
     def __getitem__(self, key):
         return self.data[key]
+    
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        data ={}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
             
 
