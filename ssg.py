618d1c2b313442e2e204dc15747df4e6f61069a2
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-14 12:35:06 +0530

    parser class

--- ssg.py
+++ ssg.py
@@ -1,8 +1,9 @@
 import typer
 from ssg.site import Site
+import ssg.parsers
 
 def main(source = "content", dest = "dist"):
-    config = {"source": source, "dest": dest}
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
     Site(**config).build()
 
 typer.run(main)
--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,3 +1,5 @@
+import shutil
+
 from typing import List
 from pathlib import Path
 
@@ -10,5 +12,26 @@ class Parser:
             return True
         else:
             return False
-            
-                
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+        
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+        
+    def write(self, path, dest, content, ext = ".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(self, path, source, dest)
+
